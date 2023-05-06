ac7e3188a0bba2aac02b400e6b20a6f8684c7176
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-07 00:09:53 +0200

    Add Song component

---
+++ src/components/Songs/Song/Song.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import classes from "./Song.module.css";
+import { replacePlayingSong } from "../../../redux/songSlice";
+import axios from '../../../myAxios'
+
+const Song = (props) => {
+  const dispatch = useDispatch()
+
+  return (
+    <div className={classes.Song} onClick={() => dispatch(replacePlayingSong(props.id))}>
+      <span className={classes.Index}>{props.index + 1}</span>
+        
+      <img className={classes.Image} src={`${axios.defaults.baseURL}albums/${props.albumID}/cover`} />
+      <span className={classes.Title}>{props.title}</span>
+      <span>{props.artist.name}</span>
+      <span>3:00</span>
+    </div>
+  );
+};
+
+export default Song;
