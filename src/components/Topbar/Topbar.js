9bcaad766a5886d7e5f3e7a64e89f4bf699d9726
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-07 04:36:24 +0200

    Add Go Back button

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -3,7 +3,7 @@ import classes from "./Topbar.module.css";
 const Topbar = (props) => {
   return (
     <div style={props.opacity} className={classes.Topbar}>
-      <button onClick={() => props.resetCards()} className={classes.Sign}>Sign up</button>
+      <button onClick={() => props.resetCards()} className={classes.Sign}>Go Back</button>
       <button className={classes.Login}>Log in</button>
     </div>
   );
