c5988ddfc09d78cad21f1d503a732349de0f3bb9
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-16 15:12:39 +0200

    Add responsiveness

--- src/components/Songs/Songs.js
+++ src/components/Songs/Songs.js
@@ -1,36 +1,63 @@
 import Song from "./Song/Song";
 import playIcon from "../../assets/play.png";
+import pauseIcon from "../../assets/pause.png";
 import classes from "./Songs.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { replacePlayingSong, selectSongs } from "../../redux/songSlice";
+import {
+  replacePlayingSong,
+  selectIsPlaying,
+  selectPlayingSong,
+  selectSongs,
+  setIsPlaying,
+} from "../../redux/songSlice";
+import { useMediaQuery } from "react-responsive";
 
 const Songs = (props) => {
   const albumSongs = useSelector(selectSongs);
+  const playingSong = useSelector(selectPlayingSong);
   const dispatch = useDispatch();
+  const isPlaying = useSelector(selectIsPlaying);
+  const isXXSm = useMediaQuery({ maxWidth: 540 });
 
   let songs = null;
   if (albumSongs.length > 0)
     songs = albumSongs.map((item, index) => (
-      <Song albumID={props.albumID} key={item.id} index={index} {...item} />
+      <Song
+        isXXSm={isXXSm}
+        albumID={props.albumID}
+        key={item.id}
+        index={index}
+        {...item}
+      />
     ));
+
+  const handleButtonClick = () => {
+    const firstSong = albumSongs[0].id;
+    if (playingSong === null) dispatch(replacePlayingSong(firstSong));
+    else dispatch(setIsPlaying(!isPlaying));
+  };
 
   return (
     <div className={classes.Songs}>
       <div className={classes.ButtonRow}>
         <button
           disabled={albumSongs.length === 0}
-          onClick={() => dispatch(replacePlayingSong(albumSongs[0].id))}
+          onClick={handleButtonClick}
           className={classes.Play}
         >
-          <img src={playIcon} />
+          <img src={!isPlaying ? playIcon : pauseIcon} />
         </button>
       </div>
       <div className={classes.Header}>
         <span className={classes.Index}>#</span>
         <span>Title</span>
-        <span></span>
-        <span>Artist</span>
-        <span>Length</span>
+        {!isXXSm ? (
+          <>
+            <span></span>
+            <span>Artist</span>
+            <span>Length</span>
+          </>
+        ) : null}
       </div>
       {songs}
     </div>
