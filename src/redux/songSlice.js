af402a0382c6e6dca24febb20c6ae22f8abdd3df
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-16 15:10:20 +0200

    Update songSlice

--- src/redux/songSlice.js
+++ src/redux/songSlice.js
@@ -3,6 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 export const songSlice = createSlice({
   initialState: {
     playingSong: null,
+    isPlaying: false,
+    currentTime: 0,
     songs: [],
   },
   name: "songs",
@@ -13,12 +15,21 @@ export const songSlice = createSlice({
     replacePlayingSong: (state, action) => {
       state.playingSong = action.payload;
     },
+    setIsPlaying: (state, action) => {
+      state.isPlaying = action.payload;
+    },
+    setCurrentTime: (state, action) => {
+      state.currentTime = action.payload
+    }
   },
 });
 
-export const { replaceSongs, replacePlayingSong } = songSlice.actions;
+export const { replaceSongs, replacePlayingSong, setIsPlaying, setCurrentTime } =
+  songSlice.actions;
 
 export const selectSongs = (state) => state.songs.songs;
 export const selectPlayingSong = (state) => state.songs.playingSong;
+export const selectIsPlaying = (state) => state.songs.isPlaying;
+export const selectCurrentTime = (state) => state.songs.currentTime
 
 export default songSlice.reducer;
