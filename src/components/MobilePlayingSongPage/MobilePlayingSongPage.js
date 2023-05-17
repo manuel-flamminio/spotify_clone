93d71d06e9edd5e976e77d2f2a8bac1a4a58df93
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-17 15:18:55 +0200

    Fix crash on mobile playing song page

--- src/components/MobilePlayingSongPage/MobilePlayingSongPage.js
+++ src/components/MobilePlayingSongPage/MobilePlayingSongPage.js
@@ -1,12 +1,10 @@
 import classes from "./MobilePlayingSongPage.module.css";
 import { useSelector } from "react-redux";
 import { selectShade } from "../../redux/uiSlice";
-import { selectAlbum } from "../../redux/albumSlice";
 
 const MobilePlayingSongPage = (props) => {
   const backgroundShade = useSelector(selectShade);
   const background = "linear-gradient(transparent 0,rgba(0,0,0,.7) 60%)";
-  const album = useSelector(selectAlbum)
 
   return (
     <div
@@ -15,7 +13,7 @@ const MobilePlayingSongPage = (props) => {
     >
       <div className={classes.Headers}>
         <button className={classes.BackButton} onClick={() => props.goBack()}>&lt; Back</button>
-        <span>{album.title}</span>
+        <span>{props.song.album.title}</span>
       </div>
       <div className={classes.PlayerControls}>{props.player}</div>
     </div>
