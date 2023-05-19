73e7afabcb5986fb6a1a87a308db067b9c972b12
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-19 14:09:02 +0200

    Remove albums from albumSlice

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,21 +1,18 @@
-import { useSelector } from "react-redux";
 import Card from "./Card/Card";
 import classes from "./Cards.module.css";
-import { selectAlbums } from "../../redux/albumSlice";
 
 const Cards = (props) => {
-  const cards = useSelector(selectAlbums)
-  const visibleItems = cards.slice(0, props.trim);
+  let cards = props.albums;
 
-  const items = visibleItems.map((item, index) => {
-    return (
-      <Card index={index} card={item} min={props.trim} key={item.id} />
-    );
+  if (props.trim > 0) cards = cards.slice(0, props.trim);
+
+  const items = cards.map((item, index) => {
+    return <Card index={index} card={item} min={props.trim} key={item.id} />;
   });
 
   return (
     <>
-      <h2 className={classes.Title}>{props.title ? props.title : 'Titolo'}</h2>
+      <h2 className={classes.Title}>{props.title ? props.title : "Albums"}</h2>
       <div className={classes.Cards}>{items}</div>
     </>
   );
--- src/redux/albumSlice.js
+++ src/redux/albumSlice.js
@@ -2,23 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const albumSlice = createSlice({
     initialState: {
-        selectedAlbum: null,
-        albums: []
+        selectedAlbum: null
     },
     name: 'album',
     reducers: {
         replaceAlbum: (state, action) => {
             state.selectedAlbum = action.payload;
-        },
-        replaceAlbums: (state, action) => {
-            state.albums = action.payload
         }
     }
 })
 
-export const {replaceAlbum, replaceAlbums} = albumSlice.actions
+export const {replaceAlbum} = albumSlice.actions
 
 export const selectAlbum = state => state.album.selectedAlbum
-export const selectAlbums = state => state.album.albums
 
 export default albumSlice.reducer
