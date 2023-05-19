d88d21b2ae83884db3b7bbc43161d5905f8380ce
manuel-flamminio <manuel.flamminio@gmail.com>
2023-05-19 14:07:44 +0200

    Add Section component

---
+++ src/components/Section/Section.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "../../myAxios";
+import Cards from "../Cards/Cards";
+
+const Section = (props) => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`sections/${props.section.id}/albums`)
+      .then((res) => setAlbums(res.data))
+      .catch((err) => console.log(err));
+  }, [props.section.id]);
+
+  return (
+    <>
+      {albums.length > 0 ? (
+        <Cards trim={props.trim} title={props.section.name} albums={albums} />
+      ) : null}
+    </>
+  );
+};
+
+export default Section;
